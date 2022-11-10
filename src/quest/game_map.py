e5f62df17c5c75ea9c8cdf68017d78f202c2f1c0
Neil Vaytet <neil.vaytet@esss.se>
2022-11-10 22:59:06 +0100

    add moving dot

--- src/quest/game_map.py
+++ src/quest/game_map.py
@@ -8,10 +8,13 @@ class Map:
     def __init__(self, nx=1920, ny=1080):
         self.nx = nx
         self.ny = ny
+        self.map = np.zeros((ny, nx))
         self.fig, self.ax = plt.subplots()
+        self.ax.set_aspect('equal')
         self.ax.set_xlim(0, nx)
         self.ax.set_ylim(0, ny)
         self._make_obstacles()
+        self._make_castle()
         self.fig.show()
 
     def _make_obstacles(self, n=30):
@@ -19,11 +22,39 @@ class Map:
         posy = np.random.random(n) * self.ny
         dx = 40
 
-        for i in range(n):
-            rect = patches.Rectangle((posx[i], posy[i]),
-                                     dx,
-                                     dx,
-                                     linewidth=1.5,
-                                     edgecolor='k',
-                                     facecolor='none')
-            self.ax.add_patch(rect)
+        for k in range(n):
+            ic = int(posx[k])
+            jc = int(posy[k])
+            for i in range(max(int(ic - 0.5 * dx), 0),
+                           min(int(ic + 0.5 * dx), self.nx)):
+                for j in range(max(int(jc - 0.5 * dx), 0),
+                               min(int(jc + 0.5 * dx), self.ny)):
+                    self.map[j, i] = 1.0
+
+            # rect = patches.Rectangle((posx[i], posy[i]),
+            #                          dx,
+            #                          dx,
+            #                          linewidth=1.5,
+            #                          edgecolor='k',
+            #                          facecolor='C0',
+            #                          alpha=0.5)
+            # self.ax.add_patch(rect)
+        # self.ax.pcolormesh(np.arange(self.nx + 1),
+        #                    np.arange(self.ny + 1),
+        #                    self.map,
+        #                    shading='auto')
+        self.im = self.ax.imshow(self.map, origin='lower')
+
+    def _make_castle(self):
+        dx = 80
+        posx = np.random.random() * 0.5 * dx + 0.5 * dx
+        posy = np.random.random() * self.ny
+
+        rect = patches.Rectangle((posx, posy),
+                                 dx,
+                                 dx,
+                                 linewidth=1.5,
+                                 edgecolor='k',
+                                 facecolor='r',
+                                 alpha=0.8)
+        self.ax.add_patch(rect)
--- src/quest/main.py
+++ src/quest/main.py
@@ -1,5 +1,35 @@
 from game_map import Map
 
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+
 if __name__ == '__main__':
-    m = Map()
+    # mpl.use('TkAgg')
+
+    nx = 1920
+    ny = 1080
+    m = Map(nx=nx, ny=ny)
+    pos = [nx - 1, 800]
+
+    line, = m.ax.plot(pos[0], pos[1], 'o')
+
+    # circle = patches.Circle(pos, 15)
+    # m.ax.add_patch(circle)
     sec = input('Let us wait for user input.')
+
+    dt = 2.0
+    sec = True
+    while line.get_xdata()[0] > 0:
+        line.set_xdata([line.get_xdata()[0] - dt])
+        # m.fig.canvas.draw_idle()
+        # print(line.get_xdata()[0])
+        plt.pause(0.001)
+
+    sec = input('Let us wait for user input.')
+
+    # while circle.center[0] > 0:
+    #     print(circle.center[0] - dt)
+    #     circle.set_center((circle.center[0] - dt, circle.center[1]))
+    #     print(circle.center)
+    #     m.fig.canvas.draw_idle()
+    #     sec = input('Let us wait for user input.')
